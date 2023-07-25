0c9a3d7bb2444c868fad2e43dfa04b680ec35d7a
Abhinandan Jha <92037313+Avi-J0506@users.noreply.github.com>
2023-07-25 11:09:59 +0530

    Question 4

---
+++ placingFlowers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int len = flowerbed.size();
+        for (int i = 0; i < len && n > 0; i++) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == len - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                n--;
+            }
+        }
+        return n == 0;
+    }
+};
